d8fa151c5c33727d49c64aa3ea4d9305424d347f
Roman Ryazantsev <roman.ryazant@gmail.com>
2013-03-12 20:50:40 +0400

    Отладка worker

--- models/worker.php
+++ models/worker.php
@@ -6,51 +6,49 @@ require_once '../lib/idiorm.php';
 ORM::configure('pgsql:host=localhost;dbname=mind_db');
 ORM::configure('username', 'mind_distiller');
 ORM::configure('password', 'qwasdf');
-ORM::configure('logging', 'true');
+ORM::configure('id_column_overrides', array(
+    'users'=>'user_id',
+    'assertions'=>'assertion_id',
+    'assessments'=>'assertion_id'
+    ));
+
+ORM::configure('logging', true);
+ORM::configure('return_result_sets', true);
+
 //ORM::get_last_query() - возвращает последний запрос
+//where_id_is('user_id')->
 
 //Добавить проверку на коллизию имени
 function add_user($name, $pass)
 {
-    $new_id=ORM::for_table('users')->max('user_id');
-    $new_id=($new_id)+1;
-
     $user=ORM::for_table('users')->create();
 
-    $user->username=$name;
-    $user->passwd=md5($pass);
-    $user->user_id=$new_id;
-
+    //$user->username=$name;
+    //$user->passwd=md5($pass);
+    
+    $user->set(array(
+    'username'=>$name,
+    'passwd'=>md5($pass)
+    ));
+    
     $user->save();
 }
 
 function add_assertion($creator_name, $text)
 {
-    $new_id=ORM::for_table('assertions')->max('assertion_id');
-    $new_id=($new_id)+1;
-
     $creator=ORM::for_table('users')->where('username', $creator_name)->find_one();
 
     $assertion=ORM::for_table('assertions')->create();
 
-    $assertion->user_id=$creator.user_id;
+    $assertion->user_id=$creator->user_id;
     $assertion->assertion=$text;
-    $assertion->assertion_id=$new_id;
 
     $assertion->save();
 }
 
-/*function add_assessment()
+function add_assessment()
 {
 }
-
-function get_user_stats()
-{
-}
-
-function get_assertion_stats()
-{
-}*/
 
 //Отладка
 add_user('Tester', 'kolobok');
