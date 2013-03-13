98a0398bca1f7cb8ba0598ad5be6aa6ce251b0a9
Roman Ryazantsev <roman.ryazant@gmail.com>
2013-03-14 00:23:16 +0400

    Добавлено получение списка вопросов, на которые ответил пользователь

--- models/worker.php
+++ models/worker.php
@@ -1,7 +1,6 @@
 <?php
 
 require_once '../lib/idiorm.php';
-//require_once '../lib/limonade.php';
 
 ORM::configure('pgsql:host=localhost;dbname=mind_db');
 ORM::configure('username', 'mind_distiller');
@@ -13,9 +12,6 @@ ORM::configure('id_column_overrides', array(
     ));
 
 ORM::configure('logging', true);
-ORM::configure('return_result_sets', true);
-
-//ORM::get_last_query() - возвращает последний запрос
 
 //Добавить проверку на коллизию имени
 function add_user($name, $pass)
@@ -40,11 +36,41 @@ function add_assertion($creator_name, $text)
     $new_assertion->save();
 }
 
-function add_assessment()
+function add_assessment($creator_name, $assertion, $assessment, $inter, $prior, $tidy, $ment)
 {
+    $creator=ORM::for_table('users')->where('username', $creator_name)->find_one();
+    
+    $new_assessment=ORM::for_table('assessments')->create();
+    
+    $new_assessment->user_id=$creator->user_id;
+    $new_assessment->assertion_id=$assertion;  
+    $assessment ? $new_assessment->assessment=1 : $new_assessment->assessment=0;
+    $new_assessment->interest=$inter;
+    $new_assessment->priority=$prior;
+    $new_assessment->tidy=$tidy;
+    if ((bool) $ment) $new_assessment->mention=$ment;
+    
+    $new_assessment->save();
+}
+
+function get_user_assessments($user_name)
+{
+    $user=ORM::for_table('users')->where('username', $user_name)->find_one();
+        
+    $result=ORM::for_table('assessments')->where('user_id', $user->user_id)->find_array();
+    
+    return $result;
 }
 
 //Отладка
+/*
+//add_user('Tester', 'kolobok');
+//add_assertion('Tester', 'Любите молочко?');
+add_assessment('Tester', 2, 1, 0.6, 0.9, 1.0, 'Балдею с молока!');
+add_assessment('Tester', 2, 0, 0.1, 0.1, 0.1, 'Дрянь молоко ваше');
+$res=get_user_assessments('Tester');
+print_r($res);
 //echo "Query:\n"; $lastq=ORM::get_last_query(); echo $lastq, "\n";
+*/
 
 ?>
