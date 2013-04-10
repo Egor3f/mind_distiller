0487b622ac019f3161b1eaa935530f115bb6787e
Sergey Smolin <SmolinSV@gmail.com>
2013-04-11 00:38:53 +0400

    id утверждения берется из url

--- views/add_assessment.html.php
+++ views/add_assessment.html.php
@@ -1,5 +1,5 @@
-<form method="post">
-  id утверждения: <input type="text" name="assertion_id"><br>
+<form method="post" action="<?=url_for('add_assessment')?>">
+  <input type="hidden" name="assertion_id" value="<?=$assertion_id?>"><br>
   Согласие: <input type="checkbox" name="assess_flag"><br>
   Интересность: <input type="text" name="assess_interest"><br>
   Важность: <input type="text" name="assess_priority"><br>
