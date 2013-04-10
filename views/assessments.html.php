5bee4922b9c67739d2b7d8ab149543c8fa11cfe8
Roman Ryazantsev <roman.ryazant@gmail.com>
2013-04-10 23:12:19 +0400

    Вывод обоснований к оценкам

--- views/assessments.html.php
+++ views/assessments.html.php
@@ -4,6 +4,15 @@
 <p> 
 Интерес: <?=$assessment->interest?><br>
 Важность: <?=$assessment->priority?><br>
-Постановка: <?=$assessment->tidy?>
+Постановка: <?=$assessment->tidy?><br>
+<?if ($assessment->rationale_id!=1)
+{
+    $rationale = Model::factory('Rationale')->find_one($assessment->rationale_id);
+    echo 'Обоснование: '; echo $rationale->rationale_text;
+}
+else
+{
+    echo 'Без обоснования';
+}?>
 </p>
 <?endforeach?>
