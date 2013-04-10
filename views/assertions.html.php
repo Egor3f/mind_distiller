3c8d68b49336699fcc95e2336c39bf58667cbc36
Sergey Smolin <SmolinSV@gmail.com>
2013-04-11 00:41:45 +0400

    в ссылки на форму добавления оценки добалены id утверждения

--- views/assertions.html.php
+++ views/assertions.html.php
@@ -1,7 +1,7 @@
 <a href="<?=url_for('add_assertion')?>">Добавить утверждение</a>
 <?foreach($assertions as $assertion):?>
 <p>
-<?=$assertion->assertion_text?><br>
-<a href="<?=url_for('add_assessment')?>">Оценить</a>
+<?=$assertion->assertion_text?> 
+<a href="<?=url_for('add_assessment/'.$assertion->assertion_id)?>">Оценить</a>
 </p>
 <?endforeach?>
