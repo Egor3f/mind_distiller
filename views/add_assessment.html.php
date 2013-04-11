a0edbe9ef65f32c7f73eacad4d7773eb607bb489
Numekaru <alexromashev@gmail.com>
2013-04-11 11:21:42 +0400

    Косметический ремонт assessment

--- views/add_assessment.html.php
+++ views/add_assessment.html.php
@@ -1,9 +1,47 @@
 <form method="post" action="<?=url_for('add_assessment')?>">
   <input type="hidden" name="assertion_id" value="<?=$assertion_id?>"><br>
-  Согласие: <input type="checkbox" name="assess_flag"><br>
-  Интересность: <input type="text" name="assess_interest"><br>
-  Важность: <input type="text" name="assess_priority"><br>
-  Постановка вопроса: <input type="text" name="assess_tidy"><br>
-  Обоснование (опционально): <input type="text" name="rationale_text"><br>
+  <table>
+	  <tr>
+		<td align="left">
+			Согласие:
+		</td> 
+		<td align="left">
+			  <input type="checkbox" name="assess_flag"><br>
+		</td>
+	</tr>
+	<tr>
+		<td align="left">
+			Интересность: 
+		</td>
+		<td align="left">
+			<input type="range" name="assess_interest" min=0 max=100 step=1 value=50 />
+		</td>
+	<tr>
+		<td align="left">
+			Важность: 
+		</td>
+		<td align="left">
+			<input type="range" name="assess_priority" min=0 max=100 step=1 value=50 />
+		</td>
+	</tr>
+	<tr>
+		<td>
+			Постановка вопроса:
+		</td>
+		<td>
+			<input type="range" name="assess_tidy" min=0 max=100 step=1 value=50 />
+		</td>
+	</tr>
+		<tr>
+		<td align="left">
+			Обоснование (опционально):
+		</td>
+		<td align="left">
+			 <input type="text" name="rationale_text">
+		</td>
+	</tr>
+   
+  
+  </table>
   <input type="submit" value="Добавить">
 </form>
