39231d42d7b3bb0f72e37f857a1b9d70f6b0488a
Roman Ryazantsev <roman.ryazant@gmail.com>
2013-08-06 16:44:44 +0400

    Дефолтная разметка перенесена на новый каркас, рефакторинг стилей

--- index.php
+++ index.php
@@ -1,44 +1,129 @@
-﻿<html>
-<head>
-<title>Главная</title>
-<link rel="stylesheet" type="text/css" href="./css/default.css" />
-</head>
-<body>
-	<div id="header">
-		<div id="up">
-			<div id="logos">
-				<img src="./img/logo.png" />
-			</div>
-			<div id="account" >
-			<br><br>
-				<p>ЭТО Я | <a href="#">Выход</a></P>
-			</div>
-		</div>
-		<div id="fast_actions">
-			<hr>
-		</div>
-	</div>
-<div id="content">
-	<div id="menu">
-	<ul>
-	<a href="profile"><li><img src="." width="32px" height="32px"><br>Профиль</a><hr>
-	<a href="index.php"><li><img src="." width="32px" height="32px"><br>Главная</a><hr>
-	<a href="profile"><li><img src="." width="32px" height="32px"><br>Профиль</a><hr>
-	<a href="profile"><li><img src="." width="32px" height="32px"><br>Профиль</a><hr>
-	<a href="profile"><li><img src="." width="32px" height="32px"><br>Профиль</a><hr>
-	<a href="profile"><li><img src="." width="32px" height="32px"><br>Профиль</a><hr>
-	<a href="profile"><li><img src="." width="32px" height="32px"><br>Профиль</a><hr>
-	<a href="profile"><li><img src="." width="32px" height="32px"><br>Профиль</a>
-	</ul>
-	</div>
-	<div id="container">
-	</div>
-	<div id="footer">
-	<center>
-		MindDistiller 2013, all rights reserved
-	</center>
-	</div>
-	</center>
-</div>
-</body>
-</html>
+<?php
+error_reporting(E_ALL);
+ini_set ('display_errors', true);
+
+require_once 'lib/limonade.php';
+require_once 'configure.php';
+require_once 'models/worker.php';
+
+function before()
+{
+    $user = isset($_SESSION['user']) ? $_SESSION['user'] : false;
+    option('user',$user);
+    set('user',$user);
+    layout('layouts/default.html.php');
+}
+
+dispatch ('login', function(){
+    return html('login.html.php');
+});
+
+dispatch('/', function() {
+    return html('main.html.php');
+});
+
+dispatch_post('login', function(){
+    $username = $_POST['username'];
+    $password = $_POST['password'];
+    $user = Model::factory('User')
+        ->where('username',$username)
+        ->where('passwd',md5($password))
+        ->find_one();
+    echo ORM::get_last_query();
+    $_SESSION['user'] = $user;
+    if ($user) {
+        flash('Сообщение','Рады видеть Вас снова!');
+        redirect('/');
+    } else { 
+        flash('Ошибка','Неправильный пароль или имя пользователя');
+        redirect('/login');
+    }
+});
+
+dispatch('logout', function(){
+    $_SESSION['user']=null;
+    redirect('/');
+});
+
+dispatch('assertions', function(){
+    $user = User::getInstance() or redirect('/login');
+    $assertions = $user->assertions()->find_many();
+    set('assertions',$assertions);
+    return html('assertions.html.php');
+});
+
+dispatch('add_assertion', function(){
+    $user = User::getInstance() or redirect('/login');
+    return html('add_assertion.html.php');
+});
+
+dispatch_post('add_assertion', function(){
+    $user = User::getInstance() or redirect('/login');
+    $assertion = Model::factory('Assertion')->create();
+    $assertion->assertion_text = $_POST['assertion_text'];
+    $assertion->user_id=$user->user_id;
+    $assertion->save();
+    redirect('/assertions');
+});
+
+dispatch('assessments', function(){
+    $user = User::getInstance() or redirect('/login');
+    set('assessments', $user->assessments()->find_many());
+    set('user', $user);
+    return html('assessments.html.php');
+});
+
+dispatch('add_assessment/:assertion_id', function(){
+    $user = User::getInstance() or redirect('/login');
+    set('assertion_id',params('assertion_id'));
+    return html('add_assessment.html.php');
+});
+
+dispatch_post('add_assessment', function(){
+    $user = User::getInstance() or redirect('/login');
+    $assessment = Model::factory('Assessment')->create();
+    $assessment->assessment=$_POST['assess_agree'];
+    $assessment->interest=$_POST['assess_interest'];
+    $assessment->priority=$_POST['assess_priority'];
+    $assessment->tidy=$_POST['assess_tidy'];
+    $assessment->user_id=$user->user_id;
+    $assessment->assertion_id=$_POST['assertion_id'];
+    if ($_POST['rationale_text'])
+    {
+        $rationale = Model::factory('Rationale')->create();
+        $rationale->rationale_text=$_POST['rationale_text'];
+        $rationale->user_id=$user->user_id;
+        $rationale->assertion_id=$_POST['assertion_id'];
+        $rationale->save();
+        $assessment->rationale_id=$rationale->rationale_id;
+    }
+    else
+    {
+        $assessment->rationale_id=1;
+    }
+    $assessment->save();
+    redirect('/assessments');
+});
+
+dispatch_get('invitations', function(){
+    $user = User::getInstance() or redirect('/login');
+    set('invitations',$user->invitations()->find_many());
+    return html('invitations.html.php');
+});
+
+dispatch_post('invitation', function(){
+    $user = User::getInstance() or redirect('/login');
+    $key=md5(time()+mt_rand());
+    $new_inv = Model::factory('Invitation')->create();
+    $new_inv->email=$_POST['email'];
+    $new_inv->invitation_brief=$_POST['invitation_brief'];
+    $new_inv->invitation_text=$_POST['invitation_text'];
+    $new_inv->invitation_key=$key;
+    $new_inv->user_id=$user->user_id;
+    $new_inv->save();
+    redirect('/invitations');
+});
+
+run();
+
+?>
--- views/layouts/default.html.php
+++ views/layouts/default.html.php
@@ -1,52 +1,60 @@
-<html>
+﻿<html>
 <head>
-	 <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+<title>Главная</title>
+<meta http-equiv="Content-Type" content="text/html" charset="utf-8">
+<link rel="stylesheet" type="text/css" href="./css/layout.css" />
+<link rel="stylesheet" type="text/css" href="./css/colors.css" />
 </head>
-
-<link rel="stylesheet" type="text/css" href="views/css/style.css" />
 <body>
-    <div class="flash">
-        <? foreach($flash as $type => $message): ?>
-            <? if (true || $type == 'Ошибка' || $type == 'Уведомление'): ?>
-                <p><?= $message ?></p>
-            <? endif ?>
-        <? endforeach ?>
-    </div>
-  
-    <div id="logo" align="center">
-		<a href="<?=url_for('')?>"><img src="views/img/logo.png"></a>
-    </div>
-    
-    <div id="navigation">
-	<center>
-		<ul>
-            <li><a href="<?=url_for('')?>" >Главная</a></li>
-            <li><a href="<?=url_for('assertions')?>" >Утверждения</a></li>
-            <li><a href="<?=url_for('assessments')?>" >Оценки</a></li> 
-            <li><a href="<?=url_for('invitations')?>" >Приглашения</a></li>
-            <?if($user):?><li><a href="<?=url_for('logout')?>" >Выход</a></li><?else:?>
-            <li><a href="<?=url_for('login')?>" >Вход</a></li><?endif?>
-		</ul>
-	</center>
-	</div>
-    <? if (isset($news)):?>
-        <div id="news">
-            <?= $news; ?>
-        </div>
-    <?endif?>
 
-    <? if (isset($login)):?>
-        <div id="login">
-            <?= $login; ?>
-        </div>
-    <?endif?>
-    
-    <? if (isset($latest_assertions)):?>
-        <div id="latest_assertions">
-            <?= $latest_assertions; ?>
-        </div>
-    <?endif?>
-    <div id="content" width=100%>
-        <?= $content; ?>
-    </div>
+<div class="flash">
+	<? foreach($flash as $type => $message): ?>
+	<? if (true || $type == 'Ошибка' || $type == 'Уведомление'): ?>
+	<p><?= $message ?></p>
+	<? endif ?>
+	<? endforeach ?>
+</div>
+	
+<div id="header">
+	<div id="up">
+		<div id="logo">
+			<img src="./img/logo.png" />
+		</div>
+		<div id="account" >
+			<br><br>
+			<p>
+				<?if($user):?>
+				Текущий профиль | <a href="<?=url_for('logout')?>" >Выход</a>
+				<?else:?>
+				<a href="<?=url_for('login')?>" >Вход</a>
+				<?endif?>
+			</p>
+		</div>
+	</div>
+	<div id="fast_actions">
+			<hr>
+	</div>
+</div>
+
+<div id="content">
+	<div id="menu">
+		<ul>
+			<a href="."><li><img src="." ><br>Мой профиль</li></a><hr>
+			<a href="<?=url_for('')?>" ><li><img src="."><br>Новости</li></a><hr>
+			<a href="<?=url_for('assertions')?>" ><li><img src="."><br>Утверждения</li></a><hr>
+			<a href="<?=url_for('assessments')?>" ><li><img src="."><br>Оценки</li></a><hr>
+			<a href="."><li><img src="."><br>Мнения</li></a><hr>
+			<a href="<?=url_for('invitations')?>" ><img src="."><br>Приглашения</li></a><hr>
+		</ul>
+	</div>
+	
+	<div id="container">
+	</div>
+
+	<div id="footer">
+		<center>Mind Distiller</center>
+	</div>
+</div>
+
 </body>
+</html>
