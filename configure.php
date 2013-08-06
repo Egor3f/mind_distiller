83acde073ea1966c96112df4a1f75aa2da770d50
Roman Ryazantsev <roman.ryazant@gmail.com>
2013-08-06 11:41:51 +0400

    Начато вливание нового варианта верстки

--- configure.php
+++ configure.php
@@ -1,8 +1,8 @@
 <?php
 require_once 'lib/idiorm.php';
 require_once 'lib/paris.php';
-ORM::configure('pgsql:host=localhost;dbname=mind_db');
-ORM::configure('username', 'mind_distiller');
-ORM::configure('password', 'qwasdf');
+ORM::configure('pgsql:host=psql.db;dbname=md_db0');
+ORM::configure('username', 'md_user');
+ORM::configure('password', '2zlke209dLaencNNSsqLq');
 ORM::configure('logging', true);
 ?>
